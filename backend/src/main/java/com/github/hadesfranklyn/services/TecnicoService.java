5302c3c2b2baa745dc396f9d1ad1d11615328e85
Franklyn <franklyn1998@live.com>
2022-01-18 21:38:51 -0300

    Exceção para Objeto não encontrado

--- backend/src/main/java/com/github/hadesfranklyn/services/TecnicoService.java
+++ backend/src/main/java/com/github/hadesfranklyn/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.github.hadesfranklyn.domain.Tecnico;
 import com.github.hadesfranklyn.repositories.TecnicoRepository;
+import com.github.hadesfranklyn.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,7 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! " + id + ", Tipo: " + Tecnico.class.getName()));
 	}
 }
---
+++ backend/src/main/java/com/github/hadesfranklyn/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,18 @@
+package com.github.hadesfranklyn.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException() {
+		super();
+	}
+
+	public ObjectNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+
+}
