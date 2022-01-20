a5e6af03406eabd8660d17050e570475a825f675
Franklyn <franklyn1998@live.com>
2022-01-20 17:05:57 -0300

    Validação de formulários

--- frontend/ordemDeServico/src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ frontend/ordemDeServico/src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,43 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Tecnico } from 'src/app/models/tecnico';
-import { TecnicoService } from 'src/app/services/tecnico.service';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { Tecnico } from "src/app/models/tecnico";
+import { TecnicoService } from "src/app/services/tecnico.service";
 
 @Component({
-  selector: 'app-tecnico-create',
-  templateUrl: './tecnico-create.component.html',
-  styleUrls: ['./tecnico-create.component.css']
+  selector: "app-tecnico-create",
+  templateUrl: "./tecnico-create.component.html",
+  styleUrls: ["./tecnico-create.component.css"],
 })
 export class TecnicoCreateComponent implements OnInit {
+  tecnico: Tecnico = {
+    id: "",
+    nome: "",
+    cpf: "",
+    telefone: "",
+  };
 
-  tecnico: Tecnico ={
-    id: '',
-    nome: 'Franklyn',
-    cpf: '393.657.784-68',
-    telefone: '(00) 90000-0000'
+  nome = new FormControl('', [Validators.minLength(3)])
+  cpf = new FormControl('', [Validators.minLength(11)])
+  telefone = new FormControl('', [Validators.minLength(11)])
+
+
+
+  constructor(private router: Router, private service: TecnicoService) {}
+
+  ngOnInit(): void {}
+
+  cancel(): void {
+    this.router.navigate(["tecnicos"]);
   }
 
-  constructor(
-    private router : Router,
-    private service: TecnicoService
-  ) { }
-
-  ngOnInit(): void {
-  }
-
-  cancel():void{
-    this.router.navigate(['tecnicos'])
-  }
-
-  create():void{
-    this.service.create(this.tecnico).subscribe((resposta )=>{
-      this.router.navigate(['tecnicos']);
-      this.service.message('Técnico criado com sucesso!')
-    }, err =>{
-      console.log(err)
-      if(err.error.error.match('já cadastrado')){
-        this.service.message(err.error.error)
+  create(): void {
+    this.service.create(this.tecnico).subscribe(
+      (resposta) => {
+        this.router.navigate(["tecnicos"]);
+        this.service.message("Técnico criado com sucesso!");
+      },
+      (err) => {
+        if (err.error.error.match("já cadastrado")) {
+          this.service.message(err.error.error);
+        } else if (
+          err.error.errors[0].message ===
+          "número do registro de contribuinte individual brasileiro (CPF) inválido"
+        ) {
+          // this.service.message(err.error.errors[0].message);
+          this.service.message("CPF inválido!");
+        }
       }
-    })
+    );
+  }
+
+  errorValidName(){
+    if(this.nome.invalid){
+      return 'O nome deve ter entre 3 e 100 caracteres!';
+    }
+    return false;
+  }
+  errorValidCpf(){
+    if(this.cpf.invalid){
+      return 'O CPF deve ter entre 11 e 15 caracteres!';
+    }
+    return false;
+  }
+  errorValidTelefone(){
+    if(this.telefone.invalid){
+      return 'O telefone deve ter entre 11 e 18 caracteres!';
+    }
+    return false;
   }
 }
