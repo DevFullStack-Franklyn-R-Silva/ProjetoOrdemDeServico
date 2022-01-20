3415bc949fe674f779dc4590d2beb77215bd1119
Franklyn <franklyn1998@live.com>
2022-01-20 11:42:03 -0300

    Componente read-all para Tecnico (Parte 1)

--- frontend/ordemDeServico/src/app/app-routing.module.ts
+++ frontend/ordemDeServico/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/components/home/home.component';
+import { TecnicoReadComponent } from './views/components/tecnico/tecnico-read/tecnico-read.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'tecnicos',
+    component: TecnicoReadComponent
   }
 ];
 
--- frontend/ordemDeServico/src/app/app.module.ts
+++ frontend/ordemDeServico/src/app/app.module.ts
@@ -24,9 +24,10 @@ import { HeaderComponent } from './views/components/template/header/header.compo
 import { FooterComponent } from './views/components/template/footer/footer.component';
 import { NavComponent } from './views/components/template/nav/nav.component';
 import { HomeComponent } from './views/components/home/home.component';
+import { TecnicoReadComponent } from './views/components/tecnico/tecnico-read/tecnico-read.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, TecnicoReadComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
